a391bc19eeb6c1f4b6565be89b2118f988360231
Alessandro Foresi <alefor@gmail.com>
2018-12-08 10:53:29 +0000

    Move respawnDialog into SQM (3DEN)

---
+++ MPMissions/vcomai_trgm2.malden/CfgRoles.hpp
@@ -0,0 +1,120 @@
+respawnOnStart = -1;
+respawnTemplates[] = {"MenuInventory","MenuPosition"};
+
+class CfgRoles
+{
+    class 15BN_TL
+    {
+        displayName = "Team Leader";
+        icon = "a3\missions_f_exp\data\img\classes\assault_ca.paa";
+    };
+
+    class 15BN_RF
+    {
+        displayName = "Rifleman";
+        icon = "a3\missions_f_exp\data\img\classes\assault_ca.paa";
+    };
+
+    class 15BN_MG
+    {
+        displayName = "MG";
+        icon = "a3\missions_f_exp\data\img\classes\assault_ca.paa";
+    };
+
+    class 15BN_GL
+    {
+        displayName = "GL";
+        icon = "a3\missions_f_exp\data\img\classes\assault_ca.paa";
+    };
+
+    class 15BN_MEDIC
+    {
+        displayName = "Medic";
+        icon = "a3\missions_f_exp\data\img\classes\assault_ca.paa";
+    };
+
+    class 15BN_EOD
+    {
+        displayName = "EOD";
+        icon = "a3\missions_f_exp\data\img\classes\assault_ca.paa";
+    };
+
+};
+
+class CfgRespawnInventory
+{
+    class 15BN_TL_D
+    {
+        displayName = "TL Desert";        
+        role = "15BN_TL";
+    };
+
+    class 15BN_TL_W
+    {
+        displayName = "TL Woodland";        
+        role = "15BN_TL";	
+    };
+
+    class 15BN_RF_D
+    {
+        displayName = "RF Desert";
+        role = "15BN_RF";
+    };
+
+    class 15BN_RF_W
+    {
+        displayName = "RF Woodland";
+        role = "15BN_RF";
+    };
+
+
+    class 15BN_MG_D
+    {
+        displayName = "MG Desert";
+        role = "15BN_MG";
+    };
+
+    class 15BN_MG_W
+    {
+        displayName = "MG Woodland";
+        role = "15BN_MG";
+    };
+
+    class 15BN_GL_D
+    {
+        displayName = "GL Desert";
+        role = "15BN_GL";
+    };
+
+    class 15BN_GL_W
+    {
+        displayName = "GL Woodland";
+        role = "15BN_GL";
+    };
+
+    class 15BN_MEDIC_D
+    {
+        displayName = "Medic Desert";
+        role = "15BN_MEDIC";
+    };
+
+    class 15BN_MEDIC_W
+    {
+        displayName = "Medic Woodland";
+        role = "15BN_MEDIC";
+    };
+
+    class 15BN_EOD_D
+    {
+        displayName = "EOD Desert";
+        role = "15BN_EOD";
+    };
+
+    class 15BN_EOD_W
+    {
+        displayName = "EOD Woodland";
+        role = "15BN_EOD";
+    };
+    
+};
+
